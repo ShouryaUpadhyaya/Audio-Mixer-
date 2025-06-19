73865418e9a5a61907147b1e388a4d4504b6b645
ShouryaUpadhyaya <131947042+ShouryaUpadhyaya@users.noreply.github.com>
2025-06-20 01:55:02 +0530

    fixed set-vol, set-storage

--- offscreen.js
+++ offscreen.js
@@ -2,8 +2,13 @@ let activeTabs = new Map();
 
 let restoreActiveTabs = async (storedActiveTabs) => {
   try {
+    console.log("in restoreActiveTabs", storedActiveTabs);
+
     for (const tabId in storedActiveTabs) {
+      console.log("tabId: ", tabId, "storedActiveTabs: ", storedActiveTabs);
+
       let streamId = storedActiveTabs[tabId].streamId;
+      let volume = storedActiveTabs[tabId].volume;
       const media = await navigator.mediaDevices.getUserMedia({
         audio: {
           mandatory: {
@@ -18,10 +23,10 @@ let restoreActiveTabs = async (storedActiveTabs) => {
       const gainNode = new GainNode(output);
       source.connect(gainNode).connect(output.destination);
       gainNode.gain.maxValue = 6;
-      gainNode.gain.value = storedActiveTabs[tabId].volume ?? 1;
+      gainNode.gain.value = volume ?? 1;
       console.log("key: ", tabId, "value: ", storedActiveTabs[tabId]);
-      activeTabs.set(tabId, storedActiveTabs[tabId]);
-      console.log("active tabs: ", message.storedActiveTabs);
+      activeTabs.set(tabId, { ...storedActiveTabs[tabId], gainNode: gainNode });
+      console.log("active tabs: ", activeTabs.get(tabId));
     }
   } catch (error) {
     console.log("error in restore active tabs :", error);
@@ -30,26 +35,30 @@ let restoreActiveTabs = async (storedActiveTabs) => {
   console.log("restrored active tab");
 };
 
+let setVolume = async (volume = 1, gainNode) => {
+  console.log("volume", volume);
+  console.log("gainNode", gainNode);
+  gainNode.gain.value = volume;
+  console.log("set the volume", gainNode);
+};
 chrome.runtime.onMessage.addListener(async (message) => {
   if (message.target !== "offscreen") return;
-
-  console.log(message);
 
   if (message.type === "restore-activeTabs") {
     let storedActiveTabs = message.storedActiveTabs;
     console.log("offscreen inside restore-activeTabs", storedActiveTabs);
-    await restoreActiveTabs();
+    await restoreActiveTabs(storedActiveTabs);
+    console.log("restrored active tabs");
   }
 
   if (message.type === "start-recording") {
     console.log("inside start recordig offscreen");
-    // console.log(message);
-
+    let streamId = message.data;
     const media = await navigator.mediaDevices.getUserMedia({
       audio: {
         mandatory: {
           chromeMediaSource: "tab",
-          chromeMediaSourceId: message.data,
+          chromeMediaSourceId: streamId,
         },
       },
     });
@@ -63,35 +72,46 @@ chrome.runtime.onMessage.addListener(async (message) => {
     gainNode.gain.value = 1;
     console.log("source", source);
     console.log("streamId", typeof message.data, message.data);
-    let streamId = message.data;
     const tabId = message.tabId;
-    activeTabs.set(tabId, { source, gainNode, streamId });
-    console.log("activeTabs: ", activeTabs);
+    console.log("storedActiveTabs: ", storedActiveTabs[tabId]);
+
+    activeTabs.set(tabId, storedActiveTabs[tabId]);
+    console.log("active tabs: ", activeTabs.get(tabId));
   }
   if (message.type === "set-vol") {
     console.log("message set-vol: ", message);
     console.log("activeTabs set-vol: ", activeTabs);
     let tabId = message.tab.id;
     console.log("tabid : ", typeof tabId, tabId);
-
-    // let tabCtr = activeTabs.get(tabId);
     let volume = message.volume / 100;
-    // console.log("tabCtr: ", tabCtr);
-    // tabCtr.gainNode.gain.value = volume;
-    // console.log(chrome.storage);
-
+    let streamId;
     let toStore = {};
     activeTabs.forEach((value, key) => {
       console.log("value:", value);
       console.log("key:", key);
-      toStore[key] = {
-        volume: value.gainNode.gain.value,
-        streamId: value.streamId,
-      };
-      console.log("toStore[key]", toStore[key]);
+      console.log(Number(key) === tabId);
+
+      if (Number(key) === tabId) {
+        console.log("volume is: ", volume);
+
+        streamId = value.streamId;
+        toStore[key] = {
+          volume: volume,
+          streamId: value.streamId,
+        };
+      } else {
+        toStore[key] = {
+          volume: value.volume,
+          streamId: value.streamId,
+        };
+      }
+
+      setVolume(volume, value.gainNode);
+
+      console.log("toStore[key]", toStore[key], "key: ", key);
     });
     let str = toStore.toString();
-    console.log(str);
+    console.log("toStore :", str);
 
     chrome.runtime.sendMessage({
       type: "set-storage",
--- service_worker.js
+++ service_worker.js
@@ -104,6 +104,20 @@ let makeDocumentOrNot = async (message) => {
         tabId: message.tabId,
       });
     }
+  } else {
+    console.log("there exists offscreen ");
+
+    console.log("start recording sent to offscreen ");
+    const streamId = await chrome.tabCapture.getMediaStreamId({
+      targetTabId: message.tabId,
+    });
+    console.log("streamId ", streamId, message);
+    chrome.runtime.sendMessage({
+      type: "start-recording",
+      target: "offscreen",
+      data: streamId,
+      tabId: message.tabId,
+    });
   }
 };
 
@@ -114,32 +128,10 @@ chrome.runtime.onMessage.addListener(async (message) => {
   if (message.type === "init-capture") {
     try {
       await makeDocumentOrNot(message);
-      // await console.log("storedActiveTabs", storedActiveTabs);
-
-      // const existingContexts = await chrome.runtime.getContexts({});
-
-      // offscreenDocument = existingContexts.find(
-      //   (c) => c.contextType === "OFFSCREEN_DOCUMENT"
-      // );
-
-      // if (!offscreenDocument) {
-      //   // Create an offscreen document.
-      //   await chrome.offscreen.createDocument({
-      //     url: "offscreen.html",
-      //     reasons: ["USER_MEDIA"],
-      //     justification: "Recording from chrome.tabCapture API",
-      //   });
-      // }
-
-      // console.log("create stream id called");
-      // Get a MediaStream for the active tab.
     } catch (error) {
       console.log("error inside service worker: ", error);
     }
   }
-  // if(message.type === "get-streamId"){
-
-  // }
   if (message.type === "set-storage") {
     console.log(message);
     chrome.storage.local.set({ activeTabs: message.activeTabs }).then(() => {
